067f5e589aa45b78902523889a2f8d02624b1b4e
User <>
2024-05-24 03:02:57 -0400

    chore: Bump version to 1.0.1

--- GTFO.LobbyExpansion/PluginInfo.cs
+++ GTFO.LobbyExpansion/PluginInfo.cs
@@ -4,5 +4,5 @@ internal static class PluginInfo
 {
     internal const string Guid = "GTFO.LobbyExpansion";
     internal const string Name = "GTFO.LobbyExpansion";
-    internal const string Version = "1.0.0";
+    internal const string Version = "1.0.1";
 }
