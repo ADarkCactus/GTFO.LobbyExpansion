d8aeef3cab73e963a853b893f92af22197fa8027
AuriRex <xaurirexx@gmail.com>
2025-02-13 17:04:05 +0100

    Fix my stupidity and some more expanding

--- GTFO.LobbyExpansion/Patches/Harmony/ElevatorRidePatch.cs
+++ GTFO.LobbyExpansion/Patches/Harmony/ElevatorRidePatch.cs
@@ -2,6 +2,7 @@ using BepInEx.Unity.IL2CPP.Utils.Collections;
 using GTFO.LobbyExpansion.Util;
 using HarmonyLib;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using UnityEngine;
 
 namespace GTFO.LobbyExpansion.Patches.Harmony;
 
@@ -29,31 +30,59 @@ public static class ElevatorRidePatch
     }
 
     [HarmonyPatch(nameof(ElevatorRide.SpawnShaftSegments))]
-    [HarmonyPrefix]
+    [HarmonyPostfix]
     [HarmonyWrapSafe]
     public static void SpawnShaftSegments__Postfix(ElevatorRide __instance)
     {
         // Seems to be only run once on startup
         L.LogExecutingMethod();
 
+        var shaftTop = __instance.m_shaftTop;
+
+        var seats = shaftTop.m_elevatorSeats;
+
+        // We need to extend this for other parts of the game's code.
+        L.Verbose($"Expanding {nameof(__instance.m_shaftTop)}.{nameof(shaftTop.m_elevatorSeats)} size from {seats.Length} to {PluginConfig.MaxPlayers}.");
+
+        shaftTop.m_elevatorSeats = new Il2CppReferenceArray<ElevatorSeat>(PluginConfig.MaxPlayers);
+
+        for (var i = 0; i < PluginConfig.MaxPlayers; i++)
+        {
+            var seatIndex = i % 4;
+            shaftTop.m_elevatorSeats[i] = seats[seatIndex];
+        }
+
+        // Needed for cage.ConnectToShaftTop() to not throw a NRE
+        var loadingArmAnims = shaftTop.m_HSULoadingArmAnims;
+
+        L.Verbose($"Expanding {nameof(__instance.m_shaftTop)}.{nameof(shaftTop.m_HSULoadingArmAnims)} size from {loadingArmAnims.Length} to {PluginConfig.MaxPlayers}.");
+
+        shaftTop.m_HSULoadingArmAnims = new Il2CppReferenceArray<Animator>(PluginConfig.MaxPlayers);
+
+        for (var i = 0; i < PluginConfig.MaxPlayers; i++)
+        {
+            var seatIndex = i % 4;
+            shaftTop.m_HSULoadingArmAnims[i] = loadingArmAnims[seatIndex];
+        }
+
         __instance.StartCoroutine(CoroutineHelpers.NextFrame(() =>
         {
             // If we don't delay this by a frame things are null for some reason,
             // even tho they're instantiated earlier in the original method
-            // Il2Cpp moment I guess ...
-            var shaftTop = __instance.m_shaftTop;
+            // Il2Cpp moment I guess ... (or working with 3+ year old mono code as reference -moment)
 
-            var seats = shaftTop.m_elevatorSeats;
+            var cage = __instance.m_elevatorCage;
 
-            // We need to extend this for other parts of the game's code.
-            L.Verbose($"Expanding {nameof(__instance.m_shaftTop)}.{nameof(shaftTop.m_elevatorSeats)} size from {seats.Length} to {PluginConfig.MaxPlayers}.");
+            var hsuAligns = cage.m_HSUAligns;
 
-            shaftTop.m_elevatorSeats = new Il2CppReferenceArray<ElevatorSeat>(PluginConfig.MaxPlayers);
+            L.Verbose($"Expanding {nameof(__instance.m_elevatorCage)}.{nameof(cage.m_HSUAligns)} size from {hsuAligns.Length} to {PluginConfig.MaxPlayers}.");
+
+            cage.m_HSUAligns = new Il2CppReferenceArray<Transform>(PluginConfig.MaxPlayers);
 
             for (var i = 0; i < PluginConfig.MaxPlayers; i++)
             {
                 var seatIndex = i % 4;
-                shaftTop.m_elevatorSeats[i] = seats[seatIndex];
+                cage.m_HSUAligns[i] = hsuAligns[seatIndex];
             }
         }).WrapToIl2Cpp());
     }
